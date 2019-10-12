75b7f415a3a593a0e9cfa1e38f2a3b171d60ea32
arvbaska <arvindnitroxp@gmail.com>
2019-10-12 12:40:01 +0100

    Update DiceAppMain.java

--- exercise3/DiceAppMain.java
+++ exercise3/DiceAppMain.java
@@ -1,8 +1,5 @@
 package exercise3;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class DiceAppMain {
@@ -10,12 +7,14 @@ public class DiceAppMain {
 	public static void main(String[] args) throws InterruptedException {
 		Scanner sc = new Scanner(System.in);
 
+		//No of tosses for dice 1
 		System.out.println("Enter the first dice roll :: ");
 		int firstDiceRoll = sc.nextInt();
 
+		//No of tosses for dice 2
 		System.out.println("Enter the second dice roll :: ");
 		int secondDiceRoll = sc.nextInt();
-		Dice dice = new Dice();
+		Dice dice = new Dice(firstDiceRoll + secondDiceRoll);
 
 		DiceApp dice1 = new DiceApp(firstDiceRoll,dice, "Dice 1");
 		DiceApp dice2 = new DiceApp(secondDiceRoll,dice, "Dice 2");
@@ -26,12 +25,6 @@ public class DiceAppMain {
 		dice1.join();
 		dice2.join();
 
-		Map<String, Integer> frequencyMap = new HashMap<>();
-
-		dice.rollArray.forEach(item -> {
-			if (!frequencyMap.containsKey(item))
-				frequencyMap.put(String.valueOf(item), Collections.frequency(dice.rollArray, item));
-		});
-		System.out.println("FREQUENCY of Items ::: " + frequencyMap);
+		System.out.println("FREQUENCY of Items ::: " + dice.getFrequency());
 	}
 }
