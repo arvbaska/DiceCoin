2cf35ee24ed21e303a506262146757a2ab3bac5e
arvbaska <arvindnitroxp@gmail.com>
2019-10-12 12:38:29 +0100

    Update Dice.java

--- exercise3/Dice.java
+++ exercise3/Dice.java
@@ -1,22 +1,45 @@
 package exercise3;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Dice {
 	private Random random;
-	public List<Integer> rollArray = new ArrayList<Integer>();
+	public Integer[] rollArray;
+	private int index = 0;
 
+	//Initialise total tosses
+	public Dice(int totalTosses) {
+		rollArray = new Integer[totalTosses];
+	}
+
+	//Random number generator
 	private int getRandomNumberInRange() {
 
 		random = new Random();
-		return random.nextInt((6 - 1) + 1) + 1;
+		return random.nextInt(6)  + 1;
 
+	}
+	
+	//Frequency for each number
+	public Map<Integer,Integer> getFrequency(){
+		Map<Integer, Integer> frequencyMap = new HashMap<>();
+		List<Integer> list = Arrays.asList(rollArray);
+		list.forEach(item -> {
+			if (!frequencyMap.containsKey(item))
+				frequencyMap.put(item, Collections.frequency(list, item));
+		});
+		
+		return frequencyMap;
 	}
 
 	synchronized public void addIntoArray(String name) {
-		rollArray.add(getRandomNumberInRange());
-		System.out.println(rollArray + " "+ name);
+		rollArray[index] = getRandomNumberInRange();
+		System.out.println(name + " : "+ rollArray[index]);
+		index++;
 	}
 }
